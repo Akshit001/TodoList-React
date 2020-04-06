a90127cc54b5fcb31c906154f2ef40cf34bc560e
Akshit Gupta <akshitguptasmart@gmail.com>
2020-04-06 00:29:14 -0400

    Adding a fiter function to fiter out the deleted list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ class App extends Component {
   }
 deleteTodo= (id) =>{
   console.log("delete invoked  " + id); 
+  const todos = this.state.todos.filter( todo => {
+    return todo.id !== id
+  });
+  this.setState({
+    todos: todos
+    // when the key and value both are same then we can go like 
+    // todos
+  })
 }
   render() {
     return (
